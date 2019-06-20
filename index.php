bdf1f590b14b6e7987e18a5201a5913a0b57f582
Poudelpuspa <hellopuspa5@gmail.com>
2019-06-20 19:07:19 +0545

    File committed

--- index.php
+++ index.php
@@ -8,11 +8,12 @@
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<script src="https://cdn.firebase.com/libs/firebaseui/3.5.2/firebaseui.js"></script>
+	<script type="text/javascript" src="admin.js"></script>
 	<link type="text/css" rel="stylesheet" href="https://cdn.firebase.com/libs/firebaseui/3.5.2/firebaseui.css" />
 	<title>Admin Panel</title>
 </head>
 <body>
-<form action="home.php">
+<form>
 	<div class="form-group">
 	<label for="exampleInputEmail1">Email address</label>
 	<input type="email" name="email" class="form-control" id="userEmail" placeholder="Enter your email address">
@@ -32,7 +33,7 @@
 	 https://firebase.google.com/docs/web/setup#config-web-app -->
 
 
-<script type="text/javascript" src="admin.js"></script>
+
 	<!-- Optional JavaScript -->
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
