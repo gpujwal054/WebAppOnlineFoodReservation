bab3958e1b99892d89feaf97d136909d511c702f
Poudelpuspa <hellopuspa5@gmail.com>
2019-06-22 15:13:37 +0545

    View food order committed

--- view_foodOrder.php
+++ view_foodOrder.php
@@ -13,14 +13,38 @@
 	<div class="container_wrapper" style="width: 100%;height: 515px">	
 	<?php include 'sidebar.php'; ?>	
 		<div class="content" style="width:70%;float:right;height:515px;background-color: #BFF908">
-			<table>
+			<table class="table table-dark">
+				<thead>
 				<tr>
-					<td style="padding: 20px">S.N</td>
-					<td style="padding: 20px">Food Name</td>
-					<td style="padding: 20px">Quantity</td>
-					<td style="padding: 20px">Price</td>
-					<td style="padding: 20px">Status</td>
+				  <th scope="col">S.N</th>
+				  <th scope="col">Name of food</th>
+				  <th scope="col">Quantity</th>
+				  <th scope="col">Table Name</th>
+				  <th scope="col">Table Location</th>
+				  <th scope="col">Price</th>
+				  <th scope="col">Status</th>
 				</tr>
+				</thead>
+				<tbody>
+				<tr>
+				  <th scope="row">1</th>
+				  <td>Mark</td>
+				  <td>Otto</td>
+				  <td>@mdo</td>
+				</tr>
+				<tr>
+				  <th scope="row">2</th>
+				  <td>Jacob</td>
+				  <td>Thornton</td>
+				  <td>@fat</td>
+				</tr>
+				<tr>
+				  <th scope="row">3</th>
+				  <td>Larry</td>
+				  <td>the Bird</td>
+				  <td>@twitter</td>
+				</tr>
+				</tbody>
 			</table>
 		</div>
 	</div>  
