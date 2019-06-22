56e44ae0f43315fa859caff2bff31268e3407f6e
Poudelpuspa <hellopuspa5@gmail.com>
2019-06-22 15:07:00 +0545

    Insert table final commit successful

--- insert_table.php
+++ insert_table.php
@@ -11,40 +11,38 @@
 	<div class="content" style="width:70%;float:right;height:515px;background-color: #BFF908">
       	<form class="col-10 offset-0 mt-5">
 		  	<div class="form-group row">
-			    <label for="enterFoodName" class="col-2">Name of table</label>
-			    <div class="col-10">
+			    <label for="enterFoodName" class="col-3">Name of table</label>
+			    <div class="col-8" style="padding-left: 0px">
 			    	<input type="text" class="form-control form-control-sm" id="tab_name" placeholder="Enter table name">
 			    	<script type="text/javascript" src="https://www.gstatic.com/firebasejs/3.3.0/firebase.js"></script>
 			    	<script type="text/javascript" src="admin.js"></script>
 				</div>
 		  	</div>
 		  	<div class="form-group row">
-		    	<label for="enterFoodCategory" class="col-2">Number of chairs</label>
-		    	<div class="col-10">
+		    	<label for="enterFoodCategory" class="col-3">Number of chairs</label>
+		    	<div class="col-8" style="padding-left: 0px">
 			    	<input type="text" class="form-control form-control-sm" id="nums_chair" placeholder="Enter number of chairs">
 			    	<script type="text/javascript" src="https://www.gstatic.com/firebasejs/3.3.0/firebase.js"></script>
 			    	<script type="text/javascript" src="admin.js"></script>
 				</div>
 		  	</div>	
 		  	<div class="form-group row">
-			    <label for="enterPrice" class="col-2">Location of table</label>
-			    <div class="col-10">
-			    	<div class="col-10">
-				    	<select class="form-control form-control-sm" id="tab_location">
-				    		<option selected="">--Select your location--</option>
-				    		<option value="first floor">First Floor</option>
-				    		<option value="second_floor">Second Floor</option>
-				    		<option value="balcony">Balcony</option>
-				    		<option value="garden">Garden </option>
-				    	</select>
-				  	</div>
-			    	<script type="text/javascript" src="https://www.gstatic.com/firebasejs/3.3.0/firebase.js"></script>
-			    	<script type="text/javascript" src="admin.js"></script>
-		    	</div>
+			    <label for="enterPrice" class="col-3">Location of table</label>
+		    	<div class="col-8" style="padding-left: 0px">
+			    	<select class="form-control form-control-sm" id="tab_location">
+			    		<option selected="">--Select your location--</option>
+			    		<option value="first floor">First Floor</option>
+			    		<option value="second_floor">Second Floor</option>
+			    		<option value="balcony">Balcony</option>
+			    		<option value="garden">Garden </option>
+			    	</select>
+			  	</div>
+		    	<script type="text/javascript" src="https://www.gstatic.com/firebasejs/3.3.0/firebase.js"></script>
+		    	<script type="text/javascript" src="admin.js"></script>
 		  	</div>
 	  		<div class="form-group row">
-	  			<label class="col-2"></label>
-	  			<div class="col-8">
+	  			<label class="col-3"></label>
+	  			<div class="col-8" style="padding-left: 0px">
 		  			<button type="submit" class="btn btn-primary" onclick="tabs()" >Submit</button>
 		  		</div>
 			</div>
