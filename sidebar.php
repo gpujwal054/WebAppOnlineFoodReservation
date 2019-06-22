3c0e82c1d8c1989a4fe81e3188cc42ec74ccf666
Poudelpuspa <hellopuspa5@gmail.com>
2019-06-22 15:08:03 +0545

    Sidebar committed

--- sidebar.php
+++ sidebar.php
@@ -10,37 +10,40 @@
 	<div class="sidebar" style="width: 30%;background-color: #6B2B23;float: left;height: 515px">
 		<ul style="padding: 0;margin-top: 5px">
 			<div class="col-10">
-				<li style="display: block"><a href="insert_product.php" style="font-family: times new roman;font-size: 24px;color: darkgreen">Insert Product</a></li>
-			</div>
-			<!--<div class="col-10">
-				<li style="display: block;"><a href="insert_category.php" style="font-family: times new roman;font-size: 24px;color: darkgreen">Insert Category</a></li>
+				<li style="display: block"><a href="insert_product.php" style="font-family: times new roman;font-size: 24px;color: white">Insert Product</a></li>
 			</div>
 			<div class="col-10">
-				<li style="display: block;"><a href="insert_subCat.php" style="font-family: times new roman;font-size: 24px;color: darkgreen">Insert Sub-Category</a></li>
-			</div>-->
-			<div class="col-10">
-				<li style="display: block;"><a href="insert_table.php" style="font-family: times new roman;font-size: 24px;color: darkgreen">Insert Table</a></li>
+				<li style="display: block;"><a href="insert_category.php" style="font-family: times new roman;font-size: 24px;color: white">Insert Category</a></li>
 			</div>
 			<div class="col-10">
-				<li style="display: block;"><a href="view_foodOrder.php" style="font-family: times new roman;font-size: 24px;color: darkgreen">View Food Order</a></li>
+				<li style="display: block;"><a href="insert_subCat.php" style="font-family: times new roman;font-size: 24px;color: white">Insert Sub-Category</a></li>
 			</div>
 			<div class="col-10">
-				<li style="display: block;"><a href="view_tableOrder.php" style="font-family: times new roman;font-size: 24px;color: darkgreen">View Table Order</a></li>
+				<li style="display: block;"><a href="insert_table.php" style="font-family: times new roman;font-size: 24px;color: white">Insert Table</a></li>
 			</div>
 			<div class="col-10">
-				<li style="display: block;"><a href="view_onlineFoodOrder.php" style="font-family: times new roman;font-size: 24px;color: darkgreen">View Online Food Order</a></li>
+				<li style="display: block;"><a href="view_foodOrder.php" style="font-family: times new roman;font-size: 24px;color: white">View Food Order</a></li>
 			</div>
 			<div class="col-10">
-				<li style="display: block;"><a href="user_accountManagement.php" style="font-family: times new roman;font-size: 24px;color: darkgreen">User Account Management</a></li>
+				<li style="display: block;"><a href="view_tableOrder.php" style="font-family: times new roman;font-size: 24px;color: white">View Table Order</a></li>
 			</div>
 			<div class="col-10">
-				<li style="display: block;"><a href="daily_incomeStatistics.php" style="font-family: times new roman;font-size: 24px;color: darkgreen">Daily Income Statistics</a></li>
+				<li style="display: block;"><a href="view_onlineFoodOrder.php" style="font-family: times new roman;font-size: 24px;color: white">View Online Food Order</a></li>
 			</div>
 			<div class="col-10">
-				<li style="display: block;"><a href="terms_conditions.php" style="font-family: times new roman;font-size: 24px;color: darkgreen">Terms and conditions</a></li>
+				<li style="display: block;"><a href="user_accountManagement.php" style="font-family: times new roman;font-size: 24px;color: white">User Account Management</a></li>
 			</div>
 			<div class="col-10">
-				<li style="display: block;"><a href="privacy.php" style="font-family: times new roman;font-size: 24px;color: darkgreen">Privacy</a></li>
+				<li style="display: block;"><a href="view_items.php" style="font-family: times new roman;font-size: 24px;color: white">View Product Items</a></li>
+			</div>
+			<div class="col-10">
+				<li style="display: block;"><a href="daily_incomeStatistics.php" style="font-family: times new roman;font-size: 24px;color: white">Daily Income Statistics</a></li>
+			</div>
+			<div class="col-10">
+				<li style="display: block;"><a href="terms_conditions.php" style="font-family: times new roman;font-size: 24px;color: white">Terms and conditions</a></li>
+			</div>
+			<div class="col-10">
+				<li style="display: block;"><a href="privacy.php" style="font-family: times new roman;font-size: 24px;color: white">Privacy</a></li>
 			</div>
 		</ul>
 	</div>
