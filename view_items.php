db3cdf4b27cc64d2d5bea6eb32b7eaeeb6feaad2
Poudelpuspa <hellopuspa5@gmail.com>
2019-06-22 15:14:19 +0545

    View items file committed

---
+++ view_items.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<title>View Product Items</title>
+	</head>
+	<body>
+		<?php include 'header.php';?>
+		<div class="container_wrapper" style="width: 100%;height: 515px">
+		<?php include 'sidebar.php'; ?>
+			<div class="content" style="width:70%;float:right;height:515px;background-color: #BFF908">
+				<table class="table table-dark">
+					<thead>
+					    <tr>
+					      <th scope="col">S.N</th>
+					      <th scope="col">Food Name</th>
+					      <th scope="col">Category</th>
+					      <th scope="col">Sub-category</th>
+					      <th scope="col">Price</th>
+					      <th scope="col">Image</th>
+					      <th scope="col">Status</th>
+					      <th scope="col">Value</th>
+					      <th scope="col">Sub-category</th>
+					    </tr>
+					</thead>
+					<tbody>
+					    <tr>
+					      <th scope="row">1</th>
+					      <td>Mark</td>
+					      <td>Otto</td>
+					      <td>@mdo</td>
+					    </tr>
+					    <tr>
+					      <th scope="row">2</th>
+					      <td>Jacob</td>
+					      <td>Thornton</td>
+					      <td>@fat</td>
+					    </tr>
+					    <tr>
+					      <th scope="row">3</th>
+					      <td>Larry</td>
+					      <td>the Bird</td>
+					      <td>@twitter</td>
+					    </tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<?php include 'footer.php';?>
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	</body>
+</html>
